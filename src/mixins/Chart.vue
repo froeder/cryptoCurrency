bdc57f133916e6df0df3a4ca949e500bff2e0e29
Jhonatan Froeder <froeder3@gmail.com>
2018-06-26 10:00:06 -0400

    include denpendences in README.md

--- src/mixins/Chart.vue
+++ src/mixins/Chart.vue
@@ -17,13 +17,13 @@ export default {
   mounted () {
     let uri = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2018-06-01?for=yesterday'
     axios.get(uri).then((response) => {
-      this.teste = response.data.bpi
+      this.responseData = response.data.bpi
       //console.log(Object.values(this.teste))
-      var teste2 = Object.entries(this.teste)
+      var responseObject = Object.entries(this.responseData)
       //console.log(teste2)
-      for (let i = 0 ; i < teste2.length ; i++){
-        this.label.push(teste2[i][0])
-        this.serie.push(teste2[i][1])
+      for (let i = 0 ; i < responseObject.length ; i++){
+        this.label.push(responseObject[i][0])
+        this.serie.push(responseObject[i][1])
       }
 
       this.renderChart({
